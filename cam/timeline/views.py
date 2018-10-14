0c7ad3205ae001290cf65dd3fc07a06facd33045
Clinton Dreisbach <clinton@dreisbach.us>
2018-10-13 20:25:26 -0400

    Add months by year sidebar again

--- cam/timeline/views.py
+++ cam/timeline/views.py
@@ -6,13 +6,17 @@ from .models import Post
 
 
 def timeline(request):
+    def last_day_of_month(any_day):
+        next_month = any_day.replace(day=28) + timedelta(days=4)  # this will never fail
+        return next_month - timedelta(days=next_month.day)
+
     offset = int(request.GET.get("offset", 0))
     limit = int(request.GET.get("limit", 10))
     start_date_str = request.GET.get("start", "")
 
     posts = Post.objects.all()
     if start_date_str:
-        start_date = datetime.strptime("%Y-%m-%d", start_date_str)
+        start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
         start_date += timedelta(1)
         posts = posts.filter(posted_at__lte=start_date)
 
@@ -29,13 +33,17 @@ def timeline(request):
         months = cursor.fetchall()
     months = [year_month[0] for year_month in months]
     months_by_year = groupby(months, lambda month: month.year)
+
     return render(
         request,
         "timeline/posts.html",
         {
             "total": total,
             "posts": posts,
-            "months_by_year": months_by_year,
+            "months_by_year": [
+                (year, [last_day_of_month(month) for month in months])
+                for year, months in months_by_year
+            ],
             "start_date_str": start_date_str,
         },
     )
