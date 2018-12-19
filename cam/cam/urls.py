769832407379f156f71ebfa6c8d82e4b3250aaa4
Clinton Dreisbach <clinton@dreisbach.us>
2018-12-19 15:26:58 -0500

    Reformat

--- cam/cam/urls.py
+++ cam/cam/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
 from django.views.generic.base import RedirectView
-
 
 from timeline import views
 
@@ -26,4 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("timeline/", views.timeline, name="timeline"),
     path("", RedirectView.as_view(url="/timeline/"), name="root"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cam/timeline/views.py
+++ cam/timeline/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
-from django.db import connection
 from datetime import datetime, timedelta
 from itertools import groupby
+
+from django.db import connection
+from django.shortcuts import render
+
 from .models import Post
 
 
 def timeline(request):
     def last_day_of_month(any_day):
-        next_month = any_day.replace(day=28) + timedelta(days=4)  # this will never fail
+        next_month = any_day.replace(day=28) + timedelta(
+            days=4)  # this will never fail
         return next_month - timedelta(days=next_month.day)
 
     offset = int(request.GET.get("offset", 0))
@@ -21,15 +24,13 @@ def timeline(request):
         posts = posts.filter(posted_at__lte=start_date)
 
     total = posts.count()
-    posts = posts[offset : offset + limit]
+    posts = posts[offset:offset + limit]
 
     with connection.cursor() as cursor:
-        cursor.execute(
-            """
-            SELECT DISTINCT(DATE_TRUNC('month', posted_at)) AS year_month 
+        cursor.execute("""
+            SELECT DISTINCT(DATE_TRUNC('month', posted_at)) AS year_month
             FROM timeline_post ORDER BY year_month DESC
-            """
-        )
+            """)
         months = cursor.fetchall()
     months = [year_month[0] for year_month in months]
     months_by_year = groupby(months, lambda month: month.year)
@@ -38,13 +39,14 @@ def timeline(request):
         request,
         "timeline/posts.html",
         {
-            "total": total,
-            "posts": posts,
-            "months_by_year": [
-                (year, [last_day_of_month(month) for month in months])
-                for year, months in months_by_year
-            ],
-            "start_date_str": start_date_str,
+            "total":
+            total,
+            "posts":
+            posts,
+            "months_by_year": [(year,
+                                [last_day_of_month(month) for month in months])
+                               for year, months in months_by_year],
+            "start_date_str":
+            start_date_str,
         },
     )
-
